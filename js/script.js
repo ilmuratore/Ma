bfbf79b3a381dd14d7bc0a849d7c6b7de89fd002
Simone Iengo <iengo.simone@gmail.com>
2024-02-19 16:30:10 +0100

    alpha 0.1 completed

---
+++ js/script.js
@@ -0,0 +1,12 @@
+// gestone script sidebar //
+
+
+const menuOpen = document.getElementById('menu-open');
+const menuClose = document.getElementById('menu-close');
+const sidebar = document.querySelector('.container .sidebar');
+
+menuOpen.addEventListener('click', () => sidebar.style.left = '0');
+
+menuClose.addEventListener('click', () => sidebar.style.left = '-100%');
+
+// work in progress 
